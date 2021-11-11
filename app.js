aa7ee333684de4bdb4795311094337db733b080a
tarwat-uddin <t.uddin9121@gmail.com>
2021-11-11 14:52:15 +0000

    Add book rating meter
    Users can now rate books from 1 to 5 to their preference

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
 const inputFormTitle = document.querySelector(".form__input--title");
 const inputFormAuthor = document.querySelector(".form__input--author");
 const inputFormPages = document.querySelector(".form__input--pages");
+const inputFormRating = document.querySelector(".form__input--rating");
 // Buttons
 const btnNewBook = document.querySelector(".btn--new-book");
 const btnSubmit = document.querySelector(".form__btn--submit");
@@ -21,57 +22,89 @@ const overlay = document.querySelector(".overlay");
 ////// Global variables
 ///////////////////////////////////////////////
 
-let myLibrary = [];
+let myLibrary;
 
 class Book {
-  constructor(title, author, pages, isRead) {
+  constructor(
+    title = "Unknown",
+    author = "Unknown",
+    pages = 0,
+    rating = 1,
+    read = false
+  ) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
+    this.rating = rating;
+    this.read = read;
   }
   toggleRead() {
-    this.isRead = !this.isRead;
+    this.read = !this.read;
   }
 }
 
-const template = new Book("Book Title", "Author Name", 0, false);
+(() => init())();
 
-addBookToLibrary(template);
+function init() {
+  myLibrary = [];
+  const template = new Book("The Maze Runner", "James Dashner", 375, 4, true);
+  addBookToLibrary(template);
+}
 
 ////////////////////////////////////////////////
 ////// App UI Setup
 ///////////////////////////////////////////////
 
 function addBookToLibrary(book) {
+  // Creating parent element
   const div = document.createElement("div");
   div.classList.add("cards__item");
   div.setAttribute("data-index", cards.childNodes.length);
-  const title = document.createElement("p");
-  title.classList.add("cards__label--title");
-  title.innerHTML = book.title;
+  // Creating child elements
+  // Book author
   const author = document.createElement("p");
   author.classList.add("cards__label--author");
   author.innerHTML = book.author;
+  // Book title label
+  const title = document.createElement("p");
+  title.classList.add("cards__label--title");
+  title.innerHTML =
+    String(book.title).length <= 48
+      ? book.title
+      : String(book.title).substring(0, 48).concat("...");
+  // Book pages label
+  const pages = document.createElement("p");
+  pages.classList.add("cards__label--pages");
+  pages.innerHTML =
+    book.pages < 4 ? book.pages : String(book.pages).substring(0, 4);
+  // Book rating meter
+  const rating = document.createElement("p");
+  rating.classList.add("cards__label--rating");
+  rating.innerHTML =
+    book.rating < 5 ? "⭐".repeat(book.rating) : "⭐".repeat(5);
+  // Remove book button
+  const remove = document.createElement("button");
+  remove.classList.add("cards__btn", "cards__btn--remove");
+  remove.innerHTML = "Remove";
+  remove.addEventListener("click", function () {
+    removeBookFromLibrary(div);
+  });
+  // Read status button
   const status = document.createElement("button");
+  status.classList.add("cards__btn", "cards__btn--status");
   status.innerHTML = "Read";
-  status.classList.add("cards__btn--status");
-  if (book.isRead) status.classList.add("cards__btn--status-active");
+  if (book.read) status.classList.add("cards__btn--status-active");
   status.addEventListener("click", function () {
     book.toggleRead();
     status.classList.toggle("cards__btn--status-active");
   });
-  const remove = document.createElement("button");
-  remove.innerHTML = "Remove Book";
-  remove.classList.add("cards__btn--remove");
-  remove.addEventListener("click", function () {
-    removeBookFromLibrary(div);
-  });
-  // Adding elements to card item
-  div.appendChild(title);
+  // Adding child elements to parent element
   div.appendChild(author);
-  div.appendChild(status);
+  div.appendChild(title);
+  div.appendChild(pages);
+  div.appendChild(rating);
   div.appendChild(remove);
+  div.appendChild(status);
   cards.appendChild(div);
 }
 
@@ -94,11 +127,18 @@ btnNewBook.addEventListener("click", function () {
 
 btnSubmit.addEventListener("click", function (e) {
   e.preventDefault();
-  const title = inputFormTitle.value;
-  const author = inputFormAuthor.value;
-  const pages = +inputFormPages.value;
-  const book = new Book(title, author, pages, false);
+  const title = inputFormTitle.value ? inputFormTitle.value : undefined;
+  const author = inputFormAuthor.value ? inputFormAuthor.value : undefined;
+  const pages = inputFormPages.value ? +inputFormPages.value : undefined;
+  const rating = inputFormRating.value ? +inputFormPages.value : undefined;
+  const book = new Book(title, author, pages, rating);
   addBookToLibrary(book);
+  // Reset input fields
+  inputFormTitle.value =
+    inputFormAuthor.value =
+    inputFormPages.value =
+    inputFormRating.value =
+      "";
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 });
